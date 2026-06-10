8f202b0f77c34cc4628112381965ae70c5ddb1df
Hellms123 <jp.hellmer@t-online.de>
2026-06-10 16:48:52 +0200

    Create lektion2.js

---
+++ lektion2.js
@@ -0,0 +1,11 @@
+let zugriffsLevel = 3;
+
+if (zugriffsLevel === 1) {
+    console.log("Gast");
+} else if (zugriffsLevel === 2) {
+    console.log("Mitarbeiter");
+} else if (zugriffsLevel === 3) {
+    console.log("Administrator");
+} else {
+    console.log("Unbekannte Rolle");
+}
